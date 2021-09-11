ff38c3cd6f7428639c9edb01fa37828c70ea473e
Zachatoo <Zachatoo@users.noreply.github.com>
2021-09-11 14:57:19 -0600

    Update cors policy
    Use regex to determine if origin is a preview deployement origin on netlify.

--- config/cors.js
+++ config/cors.js
@@ -1,13 +1,13 @@
 const allowList = [
-  'http://localhost:3000',
-  'https://zachyoung.dev',
-  'https://app.netlify.com',
+  /http:\/\/localhost:3000/,
+  /https:\/\/zachyoung.dev/,
+  /https:\/\/deploy-preview-\d+--zachatoo\.netlify\.app/,
 ];
 
 export const corsOptions = {
   credentials: true,
   origin: (origin, callback) => {
-    if (allowList.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowList.some(allowedOrigin => allowedOrigin.test(origin))) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
